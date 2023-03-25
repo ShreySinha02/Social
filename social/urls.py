4d0e0352964eb602aa5c9a2095ae5901ede8a83b
Shrey <53387208+ShreySinha02@users.noreply.github.com>
2023-03-25 18:27:37 +0530

    updated url

--- social/urls.py
+++ social/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import path,re_path,include
 from . import views
