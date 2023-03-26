cf3ab25149c3be7341f836b7e955c0ba75d20818
Shrey <53387208+ShreySinha02@users.noreply.github.com>
2023-03-26 23:36:35 +0530

    commit

--- social/settings.py
+++ social/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-x=q$5$kl$d$iperp$q1e+mgr4yzfeer_b2s!0)dhm8oq7+f+wp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
